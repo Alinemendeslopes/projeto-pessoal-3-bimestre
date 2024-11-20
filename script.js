60921dffacdd2db5d655fefbd99b8c403e85bbbe
Alinemendeslopes <aline2024mendes@gmail.com>
2024-11-20 16:10:42 -0300

    aa

---
+++ script.js
@@ -0,0 +1,29 @@
+function academy() {
+    return ('olá, bem vindo(a) a academia genasfitnes')
+}
+
+let menu = ''
+const option = [
+    '1- Fazer mobilidade.',
+    '2- Pegar mais peso ',
+    '3- Pegar menos peso.',
+    '4- Sair.'
+]
+do {
+    prompt(`Opções disponíveis:\n${options.join('\n')}`)
+    switch(menu){
+        case'1':
+        alert('Parabéns você fez a mobilidade e não ficou dolorido(a)!')
+        break
+        case'2':
+        alert('Você pegou mais peso e ficou bombadão!')
+        case'3':
+        alert('Você pegou menos peso e ficou fraco!')
+        case'4':
+        alert('Saindo...')
+        break
+        default:
+            alert('Opção indisponível.')
+    }
+
+} while(menu !=="4")
